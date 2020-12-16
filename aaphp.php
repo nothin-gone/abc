ae1514f485d2fbd557cab9ea39f9e41c06e69ea3
nothingone <nothin_gone@yahoo.com>
2020-12-17 03:09:45 +0900

    aa

---
+++ aaphp.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>aaphp</title>
+</head>
+<body>
+  <%php
+    $_POST["namae"]
+  %>
+  </br>
+  <%php
+    $_POST["myouzi"]
+  %>
+</body>
+</html>
